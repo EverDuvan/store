f9b6fbfe94c7680fdf7ec41fcf6e4dee937d231d
everduvan <everduvanhdez@gmail.com>
2024-05-12 22:00:27 -0500

    update

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+from django.contrib.auth.models import User
+
+admin.site.register(Product)
+admin.site.register(Tags)
+admin.site.register(Color)
+admin.site.register(Size)
+admin.site.register(Brand)
+admin.site.register(Category)
+admin.site.register(BrandImage)
+admin.site.register(ProductImage)
+admin.site.register(Subcategory)
+admin.site.register(CategoryImage)
+admin.site.register(SubcategoryImage)
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,133 @@
 from django.db import models
 
-# Create your models here.
+class SubcategoryImage(models.Model):
+    id = models.AutoField(primary_key=True)
+    url = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='product_images')
+    description = models.TextField()
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+class CategoryImage(models.Model):
+    id = models.AutoField(primary_key=True)
+    url = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='product_images')
+    description = models.TextField()
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    Category_image = models.ForeignKey(CategoryImage, on_delete=models.CASCADE)
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+class Subcategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    subcategory_image = models.ForeignKey(SubcategoryImage, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+class ProductImage(models.Model):
+    id = models.AutoField(primary_key=True)
+    url = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='product_images')
+    description = models.TextField()
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+class BrandImage(models.Model):
+    id = models.AutoField(primary_key=True)
+    url = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='product_images')
+    description = models.TextField()
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    url = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    brand_image = models.ForeignKey(BrandImage, on_delete=models.CASCADE)
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+class Size(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+class Color(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+class Tags(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+    product_image = models.ForeignKey(ProductImage, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    tags = models.ForeignKey(Tags, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    in_stock = models.FloatField()
+    is_active = models.BooleanField()
+    price = models.FloatField()
+    discount = models.FloatField()
+    coin = models.CharField(max_length=255)
+    sku = models.CharField(max_length=255)
+    ean = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    def __str__(self):
+        return self.name
+
+
